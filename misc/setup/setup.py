eff9d70747726337fd6211c21cc611214cd1627c
silent-sea1119 <sandragonzales56t@gmail.com>
2017-11-11 00:11:11 +0000

    Fixed typo in setup.py

--- misc/setup/setup.py
+++ misc/setup/setup.py
@@ -11,7 +11,7 @@ setup(
    author='Konstantin Kowalski',
    author_email='kostelkow@gmail.com',
    packages=['utiltools', 'utiltools.dbutils'], #, 'utiltools.bfsrs'], #, 'utiltools.shellutils'],
-   package_dir={'utiltools': '../../src', 'dbutils': '../../src/dbutils'] #, 'bfsrs':'../../src/bfsrs'},
+   package_dir={'utiltools': '../../src', 'dbutils': '../../src/dbutils'}, #, 'bfsrs':'../../src/bfsrs'},
    description='Misc tools',
    install_requires=[
       'gunicorn', 'user_agents', 'sh',
