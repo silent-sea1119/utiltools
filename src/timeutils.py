878f1e29ab4bfb4cece9502c5d86f05f94f1ed36
silent-sea1119 <sandragonzales56t@gmail.com>
2018-01-25 17:11:29 -0500

    Fixed import bugs

--- src/timeutils.py
+++ src/timeutils.py
@@ -1,5 +1,5 @@
 from datetime import timedelta, datetime, time
-
+import datetime as dt
 
 def get_day_start(dt):
    '''Given datetime, returns beginning of the day'''
@@ -27,24 +27,24 @@ def date_to_datetime(date):
 
 def datetime_from_str(str_dt):
    return datetime.strptime(str_dt, '%Y-%m-%d %H:%M:%S.%f')
-
+import time as tm
 
 def datetime_to_unix(d):
-   return time.mktime(d.timetuple())
+   return tm.mktime(d.timetuple())
 
 def unix_to_datetime(x):
-   return datetime.datetime.fromtimestamp(int(x))
+   return datetime.fromtimestamp(int(x))
 
 
 
-def datetime_to_utc_tstamp(dt):
-   return datetime_to_unix(dt)
+#def datetime_to_utc_tstamp(dt):
+#   return datetime_to_unix(dt)
 
 def datetime_now():
-   return datetime.datetime.now()
+   return datetime.now()
 
 def datetime_utc_now():
-   return datetime.datetime.now()
+   return datetime.utcnow()
 
 def tstamp_to_utc_datetime(tstamp):
    return datetime.utcfromtimestamp(tstamp)
